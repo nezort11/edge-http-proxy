c66045ac56c61c1ad2956c9764e3af9f434ff642
nezort11 <nezort11@gmail.com>
2024-10-11 01:47:19 +0300

    feat: Make edge http proxy in deno

---
+++ README.md
@@ -0,0 +1,7 @@
+# edge http proxy
+
+Simple, lighting fast edge http request proxy server written in Deno/Typescript
+
+`https://ehp.deno.dev/<Any URL>`
+
+> e.g. https://ehp.deno.dev/https://example.com/
---
+++ main.ts
@@ -0,0 +1,15 @@
+const handleProxyHttpRequest = async (_req: Request) => {
+  const originUrlObj = new URL(_req.url);
+
+  if (originUrlObj.pathname.length === 1) {
+    return new Response(
+      "Welcome to the Edge Http Proxy! Put any URL you want to proxy in path"
+    );
+  }
+
+  const targetUrl = originUrlObj.pathname.slice(1);
+  const responseProxied = await fetch(targetUrl, _req);
+  return responseProxied;
+};
+
+Deno.serve(handleProxyHttpRequest);
