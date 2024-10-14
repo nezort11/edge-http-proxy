d5457ddb7cda834da89fb3d22523686e8c5dfabe
nezort11 <nezort11@gmail.com>
2024-10-15 01:03:07 +0300

    feat: Disable cors protection from all origins

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "*",
+};
+
 const handleProxyHttpRequest = async (_req: Request) => {
+  if (_req.method === "OPTIONS") {
+    return new Response("Hi!", { headers: corsHeaders });
+  }
   const originUrlObj = new URL(_req.url);
 
   if (originUrlObj.pathname.length === 1) {
@@ -9,6 +18,9 @@ const handleProxyHttpRequest = async (_req: Request) => {
 
   const targetUrl = originUrlObj.pathname.slice(1) + originUrlObj.search;
   const responseProxied = await fetch(targetUrl, _req);
+  for (const corsHeader of Object.entries(corsHeaders)) {
+    responseProxied.headers.set(...corsHeader);
+  }
   return responseProxied;
 };
 
